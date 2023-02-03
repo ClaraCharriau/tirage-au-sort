fb032fb324f9877e5f4b12c44036d6c91a120f2c
Clara Charriau <charriau.clara@gmail.com>
2023-02-03 15:44:50 +0100

    creation mock classmate

---
+++ src/app/mocks/classmate.mocks.ts
@@ -0,0 +1,99 @@
+export interface Person {
+    id: number,
+    lastName: string,
+    genre: string,
+}
+
+export const CLASSMATES: Person[] = [
+    {
+        id: 1,
+        lastName: "Anne",
+        genre: "Femme"
+    },
+    {
+        id: 2,
+        lastName: "Hazar",
+        genre: "Femme"
+    },
+    {
+        id: 3,
+        lastName: "Thomas",
+        genre: "Homme"
+    },
+    {
+        id: 4,
+        lastName: "Fama",
+        genre: "Femme"
+    },
+    {
+        id: 5,
+        lastName: "Jiyun",
+        genre: "Femme"
+    },
+    {
+        id: 6,
+        lastName: "Assa",
+        genre: "Femme"
+    },
+    {
+        id: 7,
+        lastName: "Fahed",
+        genre: "Homme"
+    },
+    {
+        id: 8,
+        lastName: "Jérôme",
+        genre: "Homme"
+    },
+    {
+        id: 9,
+        lastName: "Liliana",
+        genre: "Femme"
+    },
+    {
+        id: 10,
+        lastName: "Diambéré",
+        genre: "Femme"
+    },
+    {
+        id: 11,
+        lastName: "Kenzia",
+        genre: "Femme"
+    },
+    {
+        id: 12,
+        lastName: "Erdal",
+        genre: "Homme"
+    },
+    {
+        id: 13,
+        lastName: "Christelle",
+        genre: "Femme"
+    },
+    {
+        id: 14,
+        lastName: "Morgane",
+        genre: "Femme"
+    },
+    {
+        id: 15,
+        lastName: "Julie",
+        genre: "Femme"
+    },
+    {
+        id: 16,
+        lastName: "Cynthia",
+        genre: "Femme"
+    },
+    {
+        id: 17,
+        lastName: "Katia",
+        genre: "Femme"
+    },
+    {
+        id: 18,
+        lastName: "Clara",
+        genre: "Femme"
+    }
+
+]
