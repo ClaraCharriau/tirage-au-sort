5900a1cf5c55a8de402a7125a7e38a3e48e06862
Clara Charriau <charriau.clara@gmail.com>
2023-02-05 19:01:07 +0100

    fonctionnement tirage au sort v1

--- src/app/components/denonciator/denonciator.component.ts
+++ src/app/components/denonciator/denonciator.component.ts
@@ -8,15 +8,53 @@ import { CLASSMATES, Person } from 'src/app/mocks/classmate.mocks';
 })
 export class DenonciatorComponent {
 
+  // Mock
   classmates: Person[] = CLASSMATES;
 
+  gameRunning: boolean = false;
+  endGame: Boolean = false;
+  availableClassmates?: Person[] = new Array();
   randomPerson?: Person;
 
-  getRandomPerson(): {} {
-    this.randomPerson = CLASSMATES[Math.floor(Math.random()* CLASSMATES.length)];
+  initGame() {
+
+    if (this.gameRunning === false) {
+      this.isGameRunning();
+      this.getAvailableClassmates();
+      this.shuffleClassmates();
+      this.getRandomPerson();
+      console.log("false", this.availableClassmates);
+
+    } else if (this.availableClassmates?.length === 0) {
+      this.endGame = true;
+
+    } else {
+      this.getRandomPerson();
+      console.log("true", this.availableClassmates);
+    }
+    
+  }
+
+
+  isGameRunning() {
+    this.gameRunning = true;
+  }
+
+  getAvailableClassmates() {
+    for (let i = 0 ; i < CLASSMATES.length ; i++ ) {
+      this.availableClassmates?.push(CLASSMATES[i]);
+    }
+  }
+
+  shuffleClassmates() {
+    this.availableClassmates?.sort(()=> Math.random() - 0.5);
+  }
+
+  getRandomPerson() {
+    this.randomPerson = this.availableClassmates?.pop();
     return this.randomPerson
   }
 
-  // TODO : ne pas nommer une personne 2x
+
 
 }
